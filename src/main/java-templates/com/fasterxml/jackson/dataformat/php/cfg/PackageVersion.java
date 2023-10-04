5eaf16c73c76ab0de101069ca133c81ec2736799
Stephan Wienczny <stephan.wienczny@ybm-deutschland.de>
2023-10-04 15:53:44 +0200

    Use templating-maven-plugin

--- src/main/java-templates/com/fasterxml/jackson/dataformat/php/cfg/PackageVersion.java
+++ src/main/java-templates/com/fasterxml/jackson/dataformat/php/cfg/PackageVersion.java
@@ -1,17 +1,17 @@
-package @package@;
+package com.fasterxml.jackson.dataformat.php.cfg;
 
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.core.Versioned;
 import com.fasterxml.jackson.core.util.VersionUtil;
 
 /**
- * Automatically generated from PackageVersion.java.in during
- * packageVersion-generate execution of maven-replacer-plugin in
+ * Automatically generated from PackageVersion.java during
+ * packageVersion-generate execution of maven-template-plugin in
  * pom.xml.
  */
 public final class PackageVersion implements com.fasterxml.jackson.core.Versioned {
     public final static Version VERSION = VersionUtil.parseVersion(
-            "@projectversion@", "@projectgroupid@", "@projectartifactid@");
+            "${project.version}", "${project.groupId}", "${project.artifactId}");
 
     @Override
     public Version version() {
